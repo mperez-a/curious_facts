74a0d4a922a52a307c7cbe387fe66422b9a0601b
Manuela Perez <manuelaperezag@gmail.com>
2023-06-07 14:45:34 +0200

    error control

--- javascript/index.js
+++ javascript/index.js
@@ -16,36 +16,58 @@ function getFact() {
 	addBtn.src = '../img/icon-star.png';
 	factText.className = 'random-text';
 	addBtn.className = 'add-btn';
+
+	let isFavorited = false;
+
 	fetch('https://uselessfacts.jsph.pl/api/v2/facts/random')
 	.then(response => response.json())
 	.then(data => {
 		factText.textContent = data.text;
 		textContainer.appendChild(factText);
 		factText.appendChild(addBtn);
-		addBtn.addEventListener('click', () => addFactToList(data.text));
+
+		addBtn.addEventListener('click', () => {
+			if(!isFavorited) {
+				addBtn.src = '../img/icon-star-fill.png';
+				isFavorited = true;
+				addFactToList(data.text);
+			} /*else {
+				addBtn.src = '../img/icon-star.png';
+				isFavorited = false;
+				removeFactFromList(data.text);
+			}*/
+		});
 	});
 }
 
 function addFactToList(text) {
-	const factsList = document.getElementById('facts-list');
-	const factItem = document.createElement('li');
-	const removeBtn = document.createElement('img');
-	removeBtn.src = '../img/icon-star-fill.png';
-	factItem.className = 'fact-item';
-	removeBtn.className = 'remove-btn';
-	factList.push(text);
-	factItem.innerHTML = text;
-	factsList.appendChild(factItem);
-	factItem.appendChild(removeBtn);
+	if (!factList.includes(text)) {
+		const factsList = document.getElementById('facts-list');
+		const factItem = document.createElement('li');
+		const removeBtn = document.createElement('img');
+	
+		removeBtn.src = '../img/icon-star-fill.png';
+		factItem.className = 'fact-item';
+		removeBtn.className = 'remove-btn';
+		factList.push(text);
+		factItem.innerHTML = text;
+		factsList.appendChild(factItem);
+		factItem.appendChild(removeBtn);
 
-	removeBtn.addEventListener('click', () => removeFactFromList(factItem));
+		removeBtn.addEventListener('click', () => removeFactFromList(factItem));
+	}
+	else {
+		alert('This fact is already in your list!');
+	}
 }
 
 function removeFactFromList(factItem) {
 	const factsList = document.getElementById("facts-list");
 	const index = Array.from(factsList.children).indexOf(factItem);
-	factList.splice(index, 1);
-	factItem.remove()
+	if (index > -1) {
+		factList.splice(index, 1);
+		factItem.remove()
+	}
 }
 
 searchBtn.addEventListener('click', getFact);
